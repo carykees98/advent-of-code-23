e444951bda8fae343fb2c845d8bdb459d14c54e6
carykees98 <trainboy980@gmail.com>
2023-12-01 13:16:15 -0500

    start of utilities

---
+++ include/aoc-includes.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+
+#include "./readInput.hpp"
+#include "./solutionPrototypes.hpp"
---
+++ include/solutionPrototypes.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+namespace aoc
+{
+	int64_t part1(const std::string &input);
+	int64_t part2(const std::string &input);
+}
