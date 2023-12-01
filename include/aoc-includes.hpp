9c3af42b5e75103dc0e68ea45353fbc65687b424
carykees98 <trainboy980@gmail.com>
2023-12-01 16:40:36 -0500

    added more includes

--- include/aoc-includes.hpp
+++ include/aoc-includes.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <algorithm>
 #include <iostream>
 #include <filesystem>
 #include <fstream>
+#include <map>
+#include <optional>
+#include <ranges>
+#include <string>
 
 #include "./readInput.hpp"
 #include "./solutionPrototypes.hpp"
